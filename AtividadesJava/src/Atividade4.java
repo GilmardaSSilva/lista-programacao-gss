8c198f9d3c5ed6205d545dab96eb67adf7ecb3b7
GilmardaSSilva <soledadesilvag@gmail.com>
2025-08-28 20:56:22 -0300

    Atividade4

---
+++ AtividadesJava/src/Atividade4.java
@@ -0,0 +1,12 @@
+public class Atividade4 {
+    public static void main(String[] args) {
+
+        final double PI = 3.1415;
+
+
+        double raio = 7.0;
+
+
+        System.out.println("PI = " + PI + ", raio = " + raio);
+    }
+}
