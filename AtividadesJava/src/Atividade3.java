a53ef184ed54ee3b5c00f62f8317a6efd62f435b
GilmardaSSilva <soledadesilvag@gmail.com>
2025-08-28 20:45:45 -0300

    Atividade3

---
+++ AtividadesJava/src/Atividade3.java
@@ -0,0 +1,15 @@
+public class Atividade3 {
+    public static void main(String[] args) {
+
+        int quantidadeAlunos = 30;
+        double mediaTurma = 7.5;
+        boolean aprovada = true;
+        String professor = "Gilmar Silva";
+
+
+        System.out.println("Quantidade de Alunos: " + quantidadeAlunos);
+        System.out.println("Média da Turma: " + mediaTurma);
+        System.out.println("Turma Aprovada: " + aprovada);
+        System.out.println("Professor: " + professor);
+    }
+}
