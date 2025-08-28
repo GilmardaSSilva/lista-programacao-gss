d0f411838daa60a5cb3f3ae927f16bad9403ca70
GilmardaSSilva <soledadesilvag@gmail.com>
2025-08-28 19:52:45 -0300

    Atividade2

---
+++ AtividadesJava/src/Atividade2.java
@@ -0,0 +1,12 @@
+public class Atividade2{
+public static void main(String[] args) {
+
+    int dia = 11;
+    int mes =03;
+    int ano = 1982;
+
+    System.out.println("dia: " + dia);
+    System.out.println("Mês: " + mes);
+    System.out.println("Ano: " + ano);
+}
+}
