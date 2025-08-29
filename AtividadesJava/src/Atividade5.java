3de8c8255f52b7b50de3469ac6cf244c58e4a021
GilmardaSSilva <soledadesilvag@gmail.com>
2025-08-28 21:03:29 -0300

    Atividade5

---
+++ AtividadesJava/src/Atividade5.java
@@ -0,0 +1,13 @@
+public static void main(String[] args) {
+
+    String curso = "Programação Java";
+    int cargaHoraria = 120;
+
+
+    System.out.println("Curso: " + curso + " – Car
+
+    }
+
+
+
+}
