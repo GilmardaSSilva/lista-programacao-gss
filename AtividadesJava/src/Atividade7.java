1a3371ffedf884e7deca6dfaa2c42b446d0f1bc5
GilmardaSSilva <soledadesilvag@gmail.com>
2025-08-28 22:01:35 -0300

    Atividade7)

---
+++ AtividadesJava/src/Atividade7.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Atividade7 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("digite o primeiro núnero (13): ");
+        int a = sc.nextInt();
+        System.out.println("Digite o segundo numero (13): ");
+        int b = sc.nextInt();
+        int soma = 13 + 13;
+
+
+    }
+}
