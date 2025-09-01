20908971cd97715934445910ec9a6ee7623d28c1
GilmardaSSilva <soledadesilvag@gmail.com>
2025-09-01 18:40:00 -0300

    Atividade8

---
+++ AtividadesJava/src/Atividade8.java
@@ -0,0 +1,17 @@
+public class Atividade8
+public class CalculoMedia {
+    public static void main(String[] args) {
+
+        double n1 = 7.5;
+        double n2 = 8.0;
+        double n3 = 6.5;
+
+
+        double media = (n1 + n2 + n3) / 3.0;
+
+        
+        System.out.println("A média das notas é: " + media);
+    }
+
+
+}
