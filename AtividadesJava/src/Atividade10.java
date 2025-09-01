0b85960f8b208cb74417a61a86ff78a8a2467621
GilmardaSSilva <soledadesilvag@gmail.com>
2025-09-01 18:58:03 -0300

    Atividade11

--- AtividadesJava/src/Atividade10.java
+++ AtividadesJava/src/Atividade10.java
@@ -7,7 +7,7 @@ public class ConversorTemperatura {
         celsius = 25.0;
 
         double fahrenheit = (9.0 * celsius) / 5.0 + 32.0;
-        
+
         System.out.println("A temperatura em Fahrenheit é: " + fahrenheit + "°F");
         {
 
---
+++ AtividadesJava/src/Atividade11.java
@@ -0,0 +1,15 @@
+public class Atividade11
+public class IdadeEmMeses {
+    public static void main(String[] args) {
+
+        int idadeAnos = 5;
+
+
+        int idadeMeses = idadeAnos * 12;
+
+
+        System.out.println("A idade em meses é: " + idadeMeses);
+    }
+}
+
+
