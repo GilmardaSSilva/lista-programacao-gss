a1185127c880a5fa9f63dba92bb057ae7136575c
GilmardaSSilva <soledadesilvag@gmail.com>
2025-09-01 19:46:13 -0300

    Atividade14

---
+++ AtividadesJava/src/Atividade14.java
@@ -0,0 +1,26 @@
+public class Atividade14
+public class AritmeticaCompleta {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+
+
+        int soma = a + b;
+        System.out.println("Soma: " + soma);
+
+
+        int subtracao = a - b;
+        System.out.println("Subtração: " + subtracao);
+        int produto = a * b;
+        System.out.println("Produto: " + produto);
+
+        double quociente = (double) a / b;
+        System.out.println("Quociente (divisão real): " + quociente);
+
+
+        int resto = a % b;
+        System.out.println("Resto (Módulo): " + resto);
+    }
+}
+
+
