ea0f74d40928ed87b9995576c4c9739428e70aa5
GilmardaSSilva <soledadesilvag@gmail.com>
2025-09-01 19:13:40 -0300

    Atividade13

---
+++ AtividadesJava/src/Atividade12.java
@@ -0,0 +1,21 @@
+public class Atividade12
+public class DivisaoInteira {
+    public static void main(String[] args) {
+        int dividendo = 17;
+        int divisor = 2;
+
+
+        int quociente = dividendo / divisor;
+
+
+        int resto = dividendo % divisor;
+
+
+        System.out.println("Dividendo: " + dividendo);
+        System.out.println("Divisor: " + divisor);
+        System.out.println("Quociente (divisão inteira): " + quociente);
+        System.out.println("Resto da divisão: " + resto);
+    }
+}
+
+
---
+++ AtividadesJava/src/Atividade13.java
@@ -0,0 +1,17 @@
+public class Atividade13
+public class ExemploPrecedencia {
+    public static void main(String[] args) {
+        // Definindo valores para as variáveis
+        int a = 5;
+        int b = 10;
+        int c = 2;
+        int d = 3;
+
+
+        int r = (a + b) * c - d;
+
+        System.out.println("O valor de r é: " + r);
+    }
+}
+
+
