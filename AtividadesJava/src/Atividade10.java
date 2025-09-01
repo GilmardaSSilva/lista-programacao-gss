0787bc82ac24a1b4d1019d23654890d86f549eec
GilmardaSSilva <soledadesilvag@gmail.com>
2025-09-01 18:52:31 -0300

    Atividade10

---
+++ AtividadesJava/src/Atividade10.java
@@ -0,0 +1,15 @@
+public class Atividade10
+public class ConversorTemperatura {
+    public static void main(String[] args) {
+
+        double celsius;
+
+        celsius = 25.0;
+
+        double fahrenheit = (9.0 * celsius) / 5.0 + 32.0;
+        
+        System.out.println("A temperatura em Fahrenheit é: " + fahrenheit + "°F");
+        {
+
+        }
+
